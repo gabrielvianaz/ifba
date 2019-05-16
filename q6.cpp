d09624b02037c8671329097a47dbf4bff1299660
gabrielvianaz <50582303+gabrielvianaz@users.noreply.github.com>
2019-05-16 09:12:22 -0300

    Update q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -5,16 +5,17 @@ int main (){
     int n1, n2, x = 1, contador = 1, aprov = 0, reprov = 0, exame = 0;
     float medias;
     
-    while (contador <= 6)
+    while (contador <= 6){
+
         
-        printf  ("Digite a nota 1 do aluno %i\n", x);
+        printf  ("\nDigite a nota 1 do aluno %i\n", x);
         scanf ("%i", &n1);
         printf ("Digite a nota 2 do aluno %i\n", x);
         scanf ("%i", &n2);
         
         int media = (n1+n2)/2;
         
-        printf ("O resultado do aluno %i foi:\n", x);
+        printf ("nO resultado do aluno %i foi:\n", x);
         x = x+1;
         
         if (media <=3){
@@ -27,13 +28,11 @@ int main (){
             printf("Exame!\n");
             exame++;
         }
-            
             contador ++;
             
         medias = medias + media;
-        
     }
-    
+       
             medias = medias/6;
         
     printf ("O total de alunos aprovados foi: %i\n", aprov);
@@ -44,4 +43,3 @@ int main (){
     return 0;
     
 }
-
